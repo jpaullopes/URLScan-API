49359155ae6f06c9cc3f89bf60916f5a8d6a36dc
jpaullopes <jpaullopes27@gmail.com>
2025-08-09 14:55:56 -0300

    minor changes

--- src/ip_formatation_indetification.py
+++ src/ip_formatation_indetification.py
@@ -2,5 +2,5 @@ from re import search
 
 # Função para verificar se uma URL contém um endereço IP
 def ip_verification(url):
-    padron =  r"^(\d{1,3}\.){3}\d{1,3}$"
+    padron =  r"(\d{1,3}\.){3}\d{1,3}"
     return search(padron,url) is not None
