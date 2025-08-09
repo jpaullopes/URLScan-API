38d509a364b60b35b858d439b517d0e1d7ecbac6
jpaullopes <jpaullopes27@gmail.com>
2025-08-09 14:23:02 -0300

    new module ip formataion

---
+++ src/ip_formatation_indetification.py
@@ -0,0 +1,6 @@
+from re import search
+
+# Função para verificar se uma URL contém um endereço IP
+def ip_verification(url):
+    padron =  r"^(\d{1,3}\.){3}\d{1,3}$"
+    return search(padron,url) is not None
