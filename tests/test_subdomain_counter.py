173a356b470a48cf08041342aa9e1c15f579966c
jpaullopes <jpaullopes27@gmail.com>
2025-09-06 19:54:51 -0300

    refactor: Remove old project structure and unused files

--- tests/test_subdomain_counter.py
+++
@@ -1,8 +0,0 @@
-from src.core.subdomain_counter import has_excessive_subdomains
-
-def test_has_excessive_subdomains():
-    url_normal = "https://www.google.com"
-    url_suspeita = "https://login.conta.bancaria.seguranca.xyz.com/entrar"
-
-    assert has_excessive_subdomains(url_normal) is False
-    assert has_excessive_subdomains(url_suspeita) is True
